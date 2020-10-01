c5e782b7b20c7fc327f2a1ed13e102b24121c08d
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-10-01 22:28:59 +0900

    add js
    add js code

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+//Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector(".itmes");
-  container.innerHTML = items.map((item) => createHTMLString(item));
+  const html = items.map((item) => createHTMLString(item));
+  container.innerHTML = items.map((item) => createHTMLString(item).join(""));
 }
 
 function createHTMLString(item) {
@@ -17,6 +19,26 @@ function createHTMLString(item) {
         <span class="item__description">${item.gender}, ${item.size}</span>
     </li>
     `;
+}
+
+function onButtonClick(event, items) {
+  const dataset = evet.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  const filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
 }
 
 //main
